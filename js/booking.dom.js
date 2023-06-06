3ae2cd8bbb0a5094810ad5903b1b06fed2c9927e
LeCodeGuy <devanj89@gmail.com>
2023-06-06 21:32:18 +0200

    added DOM references, added eventlisteners,instantiated the factory function and creates empty functions for event listeners

--- js/booking.dom.js
+++ js/booking.dom.js
@@ -1,7 +1,33 @@
 // DOM element(s) references
+const departmentElem = document.getElementById("departments"); 
+const dateElem = document.getElementById("appointmentDate");
+const nextBtnElem = document.querySelector(".nextBtn");
+const nameSurnameElem = document.querySelector("#nameSurname");
+const genderElem = document.getElementById("gender");
+const ageElem = document.querySelector("#age");
+const contactNumberElem = document.querySelector("#contactNumber");
+const emailAddressElem = document.querySelector("#email");
+const idNumberElem = document.querySelector("#idNumber");
+const backBtnElem = document.querySelector(".backBtn");
+const bookNowBtnElem = document.querySelector(".bookNowBtn");
+
+// *Testing the DOM References
+    console.log(departmentElem);
+    console.log(dateElem);
+    console.log(nextBtnElem);
+    console.log(nameSurnameElem);
+    console.log(genderElem);
+    console.log(ageElem);
+    console.log(contactNumberElem);
+    console.log(emailAddressElem);
+    console.log(idNumberElem);
+    console.log(backBtnElem);
+    console.log(bookNowBtnElem);
+// *End of testing
 
 // Add event listener to button
 
 // Instantiate the factory function
 
 // DOM events
+
