c6cabf7b9c90f292c2c568130d19578bd6ef3338
Avile Sindelo <avilesindelo@gmail.com>
2023-06-06 13:44:06 +0200

    Capitalizing the factory function name, indentation

--- js/booking.js
+++ js/booking.js
@@ -1,44 +1,41 @@
-function booking(){
+function Booking(){
 
 
 
 //function  for sending email
 function  sendMail(name,fromName,email,subject,message){
-	
-var params={
-name: name,
-from_name: "Baragwanath Hospital Management",
-email: email,
-subject:subject,
-message: message
-}
+        
+    var params={
+        name: name,
+        from_name: "Baragwanath Hospital Management",
+        email: email,
+        subject:subject,
+        message: message
+    }
 
-const serviceID="service_jjy4npj";
-const templateID="template_nm641b6";
+    const serviceID="service_jjy4npj";
+    const templateID="template_nm641b6";
 
-emailjs.send(serviceID,templateID,params).
-then(
-res=>{
-name.value="";
-email.value="";
-message.value="";
-console.log(res);
+    emailjs.send(serviceID,templateID,params).
+    then(
+        res=>{
+        name.value="";
+        email.value="";
+        message.value="";
+        console.log(res);
 
-alert("email sent successfully!");
+        alert("email sent successfully!");
 
-return "sent successfully";
-}
+        return "sent successfully";
+        }
 
-).catch((err)=>
-
-console.log(err));
-
+    ).catch((err)=> console.log(err));
+        
 }
 
 
 return{
-
-sendMail
+    sendMail
 }
 
 
