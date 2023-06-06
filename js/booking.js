97b6ee87a52e3b3f879496ba13ed8db13d687166
KayDrew <kdtshehla@gmail.com>
2023-06-06 12:50:05 +0200

    added email function

---
+++ js/booking.js
@@ -0,0 +1,45 @@
+function booking(){
+
+
+
+//function  for sending email
+function  sendMail(name,fromName,email,subject,message){
+	
+var params={
+name: name,
+from_name: "Baragwanath Hospital Management",
+email: email,
+subject:subject,
+message: message
+}
+
+const serviceID="service_jjy4npj";
+const templateID="template_nm641b6";
+
+emailjs.send(serviceID,templateID,params).
+then(
+res=>{
+name.value="";
+email.value="";
+message.value="";
+console.log(res);
+
+alert("email sent successfully!");
+
+return "sent successfully";
+}
+
+).catch((err)=>
+
+console.log(err));
+
+}
+
+
+return{
+
+sendMail
+}
+
+
+}
