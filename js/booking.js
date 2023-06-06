0d9d76ce98c181c0f046287d0bfb12876c670f97
Avile Sindelo <avilesindelo@gmail.com>
2023-06-06 13:47:39 +0200

    Proper indentation of the factory

--- js/booking.js
+++ js/booking.js
@@ -1,42 +1,39 @@
 function Booking(){
 
-
-
-//function  for sending email
-function  sendMail(name,fromName,email,subject,message){
-        
-    var params={
-        name: name,
-        from_name: "Baragwanath Hospital Management",
-        email: email,
-        subject:subject,
-        message: message
-    }
-
-    const serviceID="service_jjy4npj";
-    const templateID="template_nm641b6";
-
-    emailjs.send(serviceID,templateID,params).
-    then(
-        res=>{
-        name.value="";
-        email.value="";
-        message.value="";
-        console.log(res);
-
-        alert("email sent successfully!");
-
-        return "sent successfully";
+    //function  for sending email
+    function  sendMail(name,fromName,email,subject,message){
+            
+        var params={
+            name: name,
+            from_name: "Baragwanath Hospital Management",
+            email: email,
+            subject:subject,
+            message: message
         }
 
-    ).catch((err)=> console.log(err));
-        
-}
+        const serviceID="service_jjy4npj";
+        const templateID="template_nm641b6";
+
+        emailjs.send(serviceID,templateID,params).
+        then(
+            res=>{
+            name.value="";
+            email.value="";
+            message.value="";
+            console.log(res);
+
+            alert("email sent successfully!");
+
+            return "sent successfully";
+            }
+
+        ).catch((err)=> console.log(err));
+            
+    }
 
 
-return{
-    sendMail
-}
-
+    return{
+        sendMail
+    }
 
 }
