4ea9ef0d9d178e805fb5da9801caf588fa058621
Avile Sindelo <avilesindelo@gmail.com>
2023-06-08 10:07:59 +0200

    qr referencing

--- js/qr.dom.js
+++ js/qr.dom.js
@@ -1,7 +1,27 @@
+//Get reference to the output elements
+const patientNames = document.querySelector('#name-surname-output');
+const patientGender = document.querySelector('#gender-output');
+const patientAge = document.querySelector('#age-output');
+const patientContact = document.querySelector('#contact-output'); console.log(patientAge.innerHTML);
+const patientEmail = document.querySelector('#email-output');
+const patientID = document.querySelector('#id-number-output');
+
+//Get reference to the patient details division
+const personalDetails = document.querySelector('#patient-details');
+
 //QR scanner
 
 function onScanSuccess(decodedText, decodedResult) {
     console.log(`Code scanned = ${decodedText}`, decodedResult);
+    //populate the patient details division with the values contained in 'decodedText' 
+    patientNames.innerHTML = decodedText.name;
+    patientGender.innerHTML = decodedText.gender;
+    patientAge.innerHTML = decodedText.age;
+    patientContact.innerHTML = decodedText.contact;
+    patientEmail.innerHTML = decodedText.email;
+    patientID.innerHTML = decodedText.id;
+    html5QrcodeScanner.clear();
+    personalDetails.style.visibility = 'visible';
 }
 var html5QrcodeScanner = new Html5QrcodeScanner(
 	"qr-reader", { fps: 10, qrbox: 250 });
